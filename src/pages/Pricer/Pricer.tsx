3bc3a533ef82aa22f2d573cde957654755422e20
Calvin Lee <leecalv@ecs.vuw.ac.nz>
2021-01-02 23:16:25 +1300

    Created Class managing PricerDisplay

--- src/pages/Pricer/Pricer.tsx
+++ src/pages/Pricer/Pricer.tsx
@@ -1,120 +1,73 @@
-import React, { useState } from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonRadio, IonRadioGroup, IonTitle } from '@ionic/react';
-import Header from '../../components/Header/Header';
-import uuid from 'uuid';
-import ShowMoreLess from '../../components/ShowMoreLess/ShowMoreLess';
+import React from 'react';
+import { CardInformation } from '../../dataManagers/DataMangerInterfaces';
+import { getPricerCards, getPricerNotes } from '../../states/PricerState';
+import PricerDisplay from './PricerDisplay';
 
 ////////////////////////////////////////////////////////
-/*Enums*/
+/*Interfaces*/
 ////////////////////////////////////////////////////////
 
-enum FilterOptions {
-  ALL, MOTION_ONLY, NO_MOTION,
+export interface CardItem {
+  info      : CardInformation,
+  isChecked : boolean
+}
+
+export interface PricerInfo {
+  cards     : CardItem[],
+  notes     : string[],
+}
+
+export function getEmptyPricerInfo() : PricerInfo {
+  return {
+    cards : [],
+    notes : [],
+  } 
 }
 
 ////////////////////////////////////////////////////////
-/*Props and State*/
+/*Class*/
 ////////////////////////////////////////////////////////
 
-/**Empty*/
-
-////////////////////////////////////////////////////////
-/*Component*/
-////////////////////////////////////////////////////////
-
-const Pricer: React.FC = () => {
+/**
+ * Sorts with Async and other constantly updating components for the Pricer Screen.
+ */
+class ResultsDisplay extends React.Component<{}, PricerInfo> {
 
   ////////////////////////
-  /*Variables*/
+  /*Constructor*/
   ////////////////////////
 
-  const settingsColor : string = "secondary";
+  constructor(props : any) {
+    super(props);
+    this.state = getEmptyPricerInfo();
+  }
 
   ////////////////////////
-  /*Hooks*/
+  /*Methods*/
   ////////////////////////
 
-  const [logsShow, setLogsShow] = useState(FilterOptions.ALL);
-  const [filtersShow, setFiltersShow] = useState(false);
+  /**
+   * Updates the Components when async results.
+   */
+  async componentDidMount() {
+
+    console.log("Component Mounting");
+    this.setState({cards: await getPricerCards()});
+    this.setState({notes: await getPricerNotes()});
+  }
 
   ////////////////////////
-  /*Return*/
+  /*Render*/
   ////////////////////////
 
-  return (
-    <IonPage>
-      
-      {/* Displays the Header */}
-      <Header headerLabel="MTG Pricer"/>
-      
-      {/* Displays the History Logs */}
-      <IonContent>
+  render() {
 
-        <IonList>
-          <IonCard color={settingsColor} onClick={() => {setFiltersShow(!filtersShow)}}>
-            <IonCardHeader>
-              <IonCardTitle>
-                {"Settings"}
-                <ShowMoreLess isShow={filtersShow}/>
-              </IonCardTitle>
-            </IonCardHeader>
-
-            {/*Either Displays the Filters or not depending on the Hook*/}
-            {filtersShow
-            ?
-            <IonCardContent >
-              <IonRadioGroup value={logsShow} onIonChange={e => setLogsShow(e.detail.value)}>
-                <IonListHeader>
-                  <IonTitle>{"Log Filter"}</IonTitle>
-                </IonListHeader>
-
-                <IonItem color={settingsColor}>
-                  <IonLabel>{"All"}</IonLabel>
-                  <IonRadio slot="start" value={FilterOptions.ALL} />
-                </IonItem>
-
-                <IonItem color={settingsColor}>
-                  <IonLabel>{"Motion Only"}</IonLabel>
-                  <IonRadio slot="start" value={FilterOptions.MOTION_ONLY} />
-                </IonItem>
-
-                <IonItem color={settingsColor}>
-                  <IonLabel>{"No Motion"}</IonLabel>
-                  <IonRadio slot="start" value={FilterOptions.NO_MOTION} />
-                </IonItem>
-              </IonRadioGroup>
-            </IonCardContent>
-            : <div></div>}
-          </IonCard>
-          
-          {/*Displays the Logs with Latest First*/}
-          <div>
-            {
-            // mqttState.logs.reverse()
-            //   .filter((currentReading : Reading) => {
-            //     if (logsShow === LogsShow.ALL) { return true; }
-            //     else if (logsShow === LogsShow.MOTION_ONLY) {
-            //       if (currentReading.motionStatus) { return true; }
-            //       else { return false; }
-            //     }
-            //     else if (logsShow === LogsShow.NO_MOTION) {
-            //       if (currentReading.motionStatus) { return false; }
-            //       else { return true; }
-            //     }
-            //     else { return true; }
-            //   })
-            //   .map((currentReading : Reading) =>
-            //     <SingleHistoryLog reading={currentReading} key={uuid.v4()}/>
-            //   )
-            }
-          </div>
-
-        </IonList>
-
-      </IonContent>
-
-    </IonPage>
-  );
+    /*Display*/ 
+    return (
+      <PricerDisplay state={this.state}/>
+    );
+  }
+ 
 };
 
-export default Pricer;
+export default ResultsDisplay;
